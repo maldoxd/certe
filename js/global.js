4940bf8ab20eef990f37c39fecf3fe719eb448cc
Leonel <maldoxd@gmail.com>
2016-08-14 20:24:38 -0400

    started project

---
+++ js/global.js
@@ -0,0 +1,5 @@
+var Monday =[['Rare Roast Beef','Oaxacan Torta'],['Tuna'], ['Mozzarella Caprese', 'Pea Falafel'],['Turkey Tower', 'Chicken Caesar']];
+var MondayDist=[20,10,20,50];
+var Tuesday =[['Shredded Sirloin Banh MI','Ham & Brie'],['Tuna'], ['ALT', 'Portobello Mushroom'],['Smoked Turkey', 'Buffalo Chicken']];
+var TuesdayDist=[20,10,20,50];
+var Week=[Monday,Tuesday];
