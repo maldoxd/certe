54f210c4ebc3e1ebc165d0e251beae8464213b91
maldoxd <maldoxd@gmail.com>
2016-08-15 23:19:27 +0000

    switch from c9, still playing with the dist

--- js/global.js
+++ js/global.js
@@ -1,4 +1,4 @@
-var Monday =[['Rare Roast Beef','Oaxacan Torta'],['Tuna'], ['Mozzarella Caprese', 'Pea Falafel'],['Turkey Tower', 'Chicken Caesar']];
+var Monday =[['Mozzarella Caprese', 'Pea Falafel'],['Rare Roast Beef','Oaxacan Torta'],['Tuna'],['Turkey Tower', 'Chicken Caesar']];
 var MondayDist=[20,10,20,50];
 var Tuesday =[['Shredded Sirloin Banh MI','Ham & Brie'],['Tuna'], ['ALT', 'Portobello Mushroom'],['Smoked Turkey', 'Buffalo Chicken']];
 var TuesdayDist=[20,10,20,50];
