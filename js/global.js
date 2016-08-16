e6c86ad7e6493c7e1a67ec119d6ea89f34b46998
Leonel <maldoxd@gmail.com>
2016-08-15 21:11:42 -0400

    final test

--- js/global.js
+++ js/global.js
@@ -1,5 +1,5 @@
 var Monday =[['Mozzarella Caprese', 'Pea Falafel'],['Rare Roast Beef','Oaxacan Torta'],['Tuna'],['Turkey Tower', 'Chicken Caesar']];
-var MondayDist=[20,10,20,50];
-var Tuesday =[['Shredded Sirloin Banh MI','Ham & Brie'],['Tuna'], ['ALT', 'Portobello Mushroom'],['Smoked Turkey', 'Buffalo Chicken']];
-var TuesdayDist=[20,10,20,50];
+var MondayDist=[20,20,10,50];
+var Tuesday =[ ['ALT', 'Portobello Mushroom'],['Shredded Sirloin Banh MI','Ham & Brie'],['Tuna'],['Smoked Turkey', 'Buffalo Chicken']];
+var TuesdayDist=[20,20,10,50];
 var Week=[Monday,Tuesday];
