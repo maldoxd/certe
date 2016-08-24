7a6d5bb0f6f0baaffbb7b692ca29ad5f5b13861d
maldoxd <maldoxd@gmail.com>
2016-08-24 20:45:11 +0000

    added tin or metal

--- js/global.js
+++ js/global.js
@@ -9,5 +9,8 @@ var ThursdayDist=[20,20,10,50];
 var Friday =[ ['Focaccia'],['Godfather','Cubano'],['Tuna Club','Lobster Roll'],['Turkey & Apple Salad', 'Grilled Breast Chicken']];
 var FridayDist=[10,20,30,40];
 var Week=[Monday,Tuesday];
-var midEastern=["Chicken Kebobs", "Beef Kebobs", " Shrimp Kebobs", "Vegetable Kebobs", "Israeli Cous Cous","Grilled Vegetables","Cucumber and chick Pea Salad", "Humus & Chips", "slice Fruits"]
+var midEastern=["Chicken Kebobs", "Beef Kebobs", " Shrimp Kebobs", "Vegetable Kebobs", "Israeli Cous Cous","Grilled Vegetables","Cucumber and Chick Pea Salad", "Humus & Chips", "Slice Fruits"]
+var midEasternPlat=[1,1,1,1,0,0,0,0,0]
 var midEasternDist =[100,100,100,100,50,50,50,75,75];
+var dispName=["Disp", "TIN"];
+var ceramicName["Ceramic", "Metal"];
